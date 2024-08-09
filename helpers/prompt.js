42813c78b48d377057d25c9fc33470350b4b212d
Erick Renaud <renaud_junior@outlook.com>
2024-08-08 22:16:14 -0400

    Added formTable
    formTable is a function that takes the rows from pool query and displays them as a table

--- helpers/prompt.js
+++ helpers/prompt.js
@@ -6,20 +6,25 @@ module.exports = async function startPrompt(pool) {
     var exit = false;
     const myChoices = [
         'Exit Employee Manager',
+        new inquirer.Separator(),
         'View All Employees',
-        'Add Employees',
-        'Update Employee Role',
-        'View All Roles',
-        'Add Role',
-        'View All Department',
-        'Add Department',
+        'View Total Utilized Budget Of A Department',
         'View Employee By Department',
         'View Employee By Manager',
+        'View All Roles',
+        'View All Department',
+        new inquirer.Separator(),
+        'Add Employees',
+        'Add Role',
+        'Add Department',
+        new inquirer.Separator(),
+        'Update Employee Role',
         'Update Employee Manager',
+        new inquirer.Separator(),
         'Delete Department(s)',
         'Delete Role(s)',
         'Delete Employee(s)',
-        'View Total Utilized Budget Of A Department'
+        new inquirer.Separator(),
     ];
     while(!exit){
         await inquirer
--- helpers/selectionLogic.js
+++ helpers/selectionLogic.js
@@ -1,12 +1,16 @@
+const formTable = require('./table')
+
 const myMap = new Map();
+
 const viewEmployeeQuery = 
 `
 SELECT
-    e.id AS employee_id,
-    e.first_name AS employee_first_name,
-	e.last_name AS  employee_last_name,
-    r.title AS employee_role,
-    CONCAT(m.first_name, ' ', m.last_name) AS manager_name
+    e.id AS id,
+    e.first_name AS first_name,
+	e.last_name AS last_name,
+    r.title AS role,
+    r.salary AS salary,
+    CONCAT(m.first_name, ' ', m.last_name) AS manager
 FROM
     employee e
 LEFT JOIN
@@ -19,18 +23,20 @@ async function viewEmployee(pool){
     const employees = [];
     try{
         await pool.query(viewEmployeeQuery, function (err, { rows }) {      
-            employees.push(rows);
-            console.log(employees)
+            formTable(rows, 'id', 'first_name', 'last_name', 'role', 'salary', 'manager')
         });
         
     } catch(err){
         console.log(err)
     }
-   
-    
+}
+
+function exitEmployeeManager(pool){
+    process.exit();
 }
 
 myMap.set('View All Employees', viewEmployee);
+myMap.set('Exit Employee Manager', exitEmployeeManager);
 
 module.exports = myMap;
 
